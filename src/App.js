87d6dbf48f25ed4ba24d6ebea5e2b4fed2bb8b67
teablack <ddaria.poda@gmail.com>
2021-12-27 15:06:28 +0100

    added background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import me from "./me.png";
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <p>Frontend Developer</p>
-      </div>
-      <div class="intro">
-        <img src={me}></img>
-        {/* <div> */}
-        <p>
-          HELLO,
-          <br />
-          I’m Daria.
-        </p>
-        {/* </div> */}
-      </div>
-      <div className="figure">
-        <div className="square">
-          <div className="arrow-down"></div>
-        </div>
+      <div className="background-container">
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        {/* <div className="clouds"></div> */}
       </div>
     </div>
   );
